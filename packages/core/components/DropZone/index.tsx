95532157a7ca68b1ad7a4db294a0be15db34554e
Federico Jorge Bonel Tozzi <federicojorgeboneltozzi@Federicos-MacBook-Pro.local>
2025-11-27 12:56:24 +0900

    perf: move renderPreview to component to avoid remounting and reduce renders

--- packages/core/components/DropZone/index.tsx
+++ packages/core/components/DropZone/index.tsx
@@ -26,6 +26,7 @@ import {
   Config,
   DragAxis,
   Metadata,
+  Overrides,
   PuckContext,
   WithPuckProps,
 } from "../../types";
@@ -63,6 +64,25 @@ export type DropZoneDndData = {
   depth: number;
   path: UniqueIdentifier[];
   isDroppableTarget: boolean;
+};
+
+const InsertPreview = ({
+  element,
+  label,
+  override,
+}: {
+  element?: Element;
+  label: string;
+  override?: Overrides["drawerItem"];
+}) => {
+  if (element) {
+    return (
+      // Safe to use this since the HTML is set by the user
+      <div dangerouslySetInnerHTML={{ __html: element.outerHTML }} />
+    );
+  }
+
+  return <DrawerItemInner name={label}>{override}</DrawerItemInner>;
 };
 
 export const DropZoneEditPure = (props: DropZoneProps) => (
@@ -155,25 +175,6 @@ const DropZoneChild = ({
 
   let label = componentConfig?.label ?? item?.type.toString() ?? "Component";
 
-  const renderPreview = useMemo(
-    () =>
-      function Preview() {
-        if (item && "element" in item && item.element) {
-          return (
-            // Safe to use this since the HTML is set by the user
-            <div dangerouslySetInnerHTML={{ __html: item.element.outerHTML }} />
-          );
-        }
-
-        return (
-          <DrawerItemInner name={label}>
-            {overrides.componentItem ?? overrides.drawerItem}
-          </DrawerItemInner>
-        );
-      },
-    [componentId, label, overrides]
-  );
-
   const defaultsProps = useMemo(
     () => ({
       ...componentConfig?.defaultProps,
@@ -231,10 +232,6 @@ const DropZoneChild = ({
   const isInserting =
     "previewType" in item ? item.previewType === "insert" : false;
 
-  if (isInserting) {
-    Render = renderPreview;
-  }
-
   return (
     <DraggableComponent
       id={componentId}
@@ -249,23 +246,37 @@ const DropZoneChild = ({
       userDragAxis={collisionAxis}
       inDroppableZone={inDroppableZone}
     >
-      {(dragRef) =>
-        componentConfig?.inline && !isInserting ? (
-          <>
-            <Render
-              {...transformedProps}
-              puck={{
-                ...transformedProps.puck,
-                dragRef,
-              }}
-            />
-          </>
-        ) : (
+      {(dragRef) => {
+        if (componentConfig?.inline && !isInserting) {
+          return (
+            <>
+              <Render
+                {...transformedProps}
+                puck={{
+                  ...transformedProps.puck,
+                  dragRef,
+                }}
+              />
+            </>
+          );
+        }
+
+        return (
           <div ref={dragRef}>
-            <Render {...transformedProps} />
+            {isInserting ? (
+              <InsertPreview
+                label={label}
+                override={overrides.componentItem ?? overrides.drawerItem}
+                element={
+                  "element" in item && item.element ? item.element : undefined
+                }
+              />
+            ) : (
+              <Render {...transformedProps} />
+            )}
           </div>
-        )
-      }
+        );
+      }}
     </DraggableComponent>
   );
 };
